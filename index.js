015bd880339572ec0bbbfb70ac94d51a6035b568
paoloBugarin <paolopaolobugarin@gmail.com>
2022-11-12 17:06:47 -0800

    fixed cors error -- added origin param

--- index.js
+++ index.js
@@ -8,12 +8,13 @@ connectDB();
 const app = express(); //init express
 require("dotenv").config();
 app.use(cookieParser());
-app.use(cors());    //to allow api connection from computer to react project
+app.use(cors({credentials: true, origin: 'http://localhost:3000'}));    //to allow api connection from computer to react project
 app.use(express.json());    // allow json data in req.body
 
 //routes
 app.get("/", (req,res) => res.send("APP IS ONLINE..."))
 app.use("/api/products", require("./routes/products"))
+app.use("/api/cart", require("./routes/cart"))
 app.use("/api/users", require("./routes/auth"))
 
 //custom errorhandling (middleware)
--- routes/cart.js
+++ routes/cart.js
@@ -1,14 +1,16 @@
 const express = require("express");
 const asyncHandler = require("express-async-handler");
 const router = express.Router();
-const Cart = require("../models/userCart");
+const UserCart = require("../models/userCart");
+const cookieJwtAuth = require("../middlewares/cookieJwtAuth");
 /*
  *  @desc       Fetch user's cart items by user id
  *  @route      GET /api/cart/:id
  *  @access     Private
  */
-router.get("/:id", asyncHandler(async (req,res) => {
-    const cart = await Cart.find({ user: req.params.id})
+router.get("/:id", cookieJwtAuth, asyncHandler(async (req,res) => {
+    const cart = await UserCart.find({'user.$id': req.params.id})
+    console.log(cart, req.params.id, req.user);
     if(cart){
         res.send(cart)
     } else {
