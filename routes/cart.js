1218b75b6027b7acb21a89a0d7dde406d2bf42ae
paoloBugarin <paolopaolobugarin@gmail.com>
2022-11-15 21:53:45 -0800

    updated /cart/add route

--- routes/cart.js
+++ routes/cart.js
@@ -3,13 +3,15 @@ const asyncHandler = require("express-async-handler");
 const router = express.Router();
 const UserCart = require("../models/userCart");
 const cookieJwtAuth = require("../middlewares/cookieJwtAuth");
+const mongoose = require("mongoose");
+
 /*
  *  @desc       Fetch user's cart items by user id
  *  @route      GET /api/cart/:id
  *  @access     Private
  */
-router.get("/:id", cookieJwtAuth, asyncHandler(async (req,res) => {
-    const cart = await UserCart.findOne({user: req.params.id})
+router.get("/", cookieJwtAuth, asyncHandler(async (req,res) => {
+    const cart = await UserCart.findOne({user: req.user.id})
     if(cart){
         res.send(cart)
     } else {
@@ -26,17 +28,23 @@ router.post("/add", cookieJwtAuth, asyncHandler(async (req,res) => {
     let cart = await UserCart.findOne({user: req.user.id})
     const { cartItems } = req.body;
     if(!cart){
-        console.log(cartItems);
         cart = await UserCart.create({
             user: req.user.id,
             cartItems
         })
         res.status(201).json(cart)
     } else {
-        console.log(cart);
-        // for(const item of cartItems){
-        //     console.log(item._id)
-        // }
+        for (const reqItem of cartItems){
+            const existingItem = cart.cartItems.find(item => item._id.toString() === reqItem._id)   //check if item is already in cart, add quantity
+            if(existingItem){
+                existingItem.quantity = parseInt(existingItem.quantity) + parseInt(reqItem.quantity)
+            } else{
+                cart.cartItems.push({...reqItem, _id: mongoose.Types.ObjectId(reqItem._id)})    //push to cart
+            }
+        }
+        cart.save()
+        res.status(201).json(cart)
+
     }
 
 }))
