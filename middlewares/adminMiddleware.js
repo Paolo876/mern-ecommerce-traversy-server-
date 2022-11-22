11de45b2b6ebcb7719041260f77a2326f92e17b2
paoloBugarin <paolopaolobugarin@gmail.com>
2022-11-21 21:46:48 -0800

    updated adminMiddleware, added delete user router on admin

--- middlewares/adminMiddleware.js
+++ middlewares/adminMiddleware.js
@@ -1,11 +1,14 @@
 const User = require("../models/userModel")
+const asyncHandler = require("express-async-handler");
 
-const adminMiddleware = async (req,res, next) => {
+const adminMiddleware = asyncHandler(async (req,res, next) => {
     const user = await User.findById(req.user.id)
     if(user.isAdmin){
         next()
     } else {
-        res.status(401).json("Not Authorized.")
+        res.status(401)
+        throw new Error("Not Authorized.")
     }
-}
+})
+
 module.exports = adminMiddleware;
--- routes/admin.js
+++ routes/admin.js
@@ -15,5 +15,20 @@ router.get("/get-users", cookieJwtAuth, adminMiddleware, asyncHandler(async (req
     res.json(users.filter(item => item._id.toString() !== req.user.id))
 }))
 
+/*  @desc       delete user by id
+ *  @route      DELETE /api/admin/delete-user/:id
+ *  @access     Private/Admin
+ */
+router.delete("/delete-user/:id", cookieJwtAuth, adminMiddleware, asyncHandler(async (req,res) => {
+    const user = await User.findById(req.params.id);
+    if(user){
+        await user.remove();
+        res.json({message: "User Removed.", id: req.params.id})
+    } else {
+        res.status(404)
+        throw new Error ('User not found')
+    }
+}))
+
 
 module.exports = router
