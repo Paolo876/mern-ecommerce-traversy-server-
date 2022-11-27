9e091fd5c803ec5c38cbb4a11a3cd02ad6fa9201
paoloBugarin <paolopaolobugarin@gmail.com>
2022-11-26 21:56:39 -0800

    updated review schema --added user objectid

--- models/reviewSchema.js
+++ models/reviewSchema.js
@@ -15,7 +15,12 @@ module.exports = mongoose.Schema(
         comment: {
             type: String,
             required: true
-        }
+        },
+        user: {
+            type: mongoose.Schema.Types.ObjectId,
+            required: true,
+            ref: "User"
+        },
     }, {
         timestamps: true
     }
