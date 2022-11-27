51fb2f61cd1b5cf3e2e4903944191ebd6b1e832f
paoloBugarin <paolopaolobugarin@gmail.com>
2022-11-27 01:13:18 -0800

    updated products route

--- routes/products.js
+++ routes/products.js
@@ -9,7 +9,7 @@ const cookieJwtAuth = require("../middlewares/cookieJwtAuth");
  *  @access     Public
  */
 router.get("/", asyncHandler(async (req,res) => {
-    const products = await Product.find({})
+    const products = await Product.find({}).populate("reviews.user", "id name email")
     res.send(products)
 }))
 
@@ -34,7 +34,7 @@ router.get("/:id", asyncHandler(async (req,res) => {
  */
 router.post("/:id/reviews", cookieJwtAuth, asyncHandler(async (req,res) => {
     const { rating, comment, name } = req.body;
-    const product = await Product.findById(req.params.id)
+    const product = await Product.findById(req.params.id).populate("reviews.user", "id name email")
 
     //check if user already created a review
     if(product){        
@@ -48,7 +48,7 @@ router.post("/:id/reviews", cookieJwtAuth, asyncHandler(async (req,res) => {
         product.reviews.push({rating, comment, name, user: req.user.id});
         product.rating = product.reviews.reduce((acc, item) => Number(item.rating) + Number(acc), 0) / product.reviews.length
         await product.save();
-        res.status(201).json({ message: "Review Successfully Added!"})
+        res.status(201).json({ message: "Review Successfully Added!", reviews: product.reviews})
     } else {
         res.status(404)
         throw new Error('Product not found.')
