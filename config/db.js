9a37fad9b455f8d7145ed7a77e02d08e31d73ef5
paoloBugarin <paolopaolobugarin@gmail.com>
2022-11-29 00:06:36 -0800

    updated config db mongoose file

--- config/db.js
+++ config/db.js
@@ -2,9 +2,10 @@ const mongoose = require("mongoose");
 const colors = require("colors");
 require("dotenv").config();
 
-const connectDB = async () => {
+const connectDB = () => {
     try {
-        const db = await mongoose.connect(process.env.MONGO_URI)
+        const db = mongoose.connect(process.env.MONGO_URI)
+        mongoose.set('bufferCommands', false);
         console.log("mongodb connected:".cyan.underline.bold, db.connection.host)
     } catch (error) {
         console.log("error:".red.underline.bold, error.message)
