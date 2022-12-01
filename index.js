0a37ef78af86c73c4cfe64091061f54d016900d0
paoloBugarin <paolopaolobugarin@gmail.com>
2022-11-30 21:13:41 -0800

    updated cors origins array

--- index.js
+++ index.js
@@ -14,7 +14,8 @@ app.use(cookieParser());
 app.use(cors(
     {
         credentials: true, 
-        origin: process.env.ORIGIN || 'http://localhost:3000',  
+        // origin: process.env.ORIGIN || 'http://localhost:3000',  
+        origin:  ["https://proshop-mern-traversy.netlify.app", "https://www.paolobugarinmernheroku.xyz", "http://localhost:3000"],
 
 }));    //to allow api connection from computer to react project
 // app.use(cors({credentials: true, origin: '*' }));    //to allow api connection from computer to react project
