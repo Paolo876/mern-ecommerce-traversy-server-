912a64454740c27eb228dec34ebb52676bf830fe
paoloBugarin <paolopaolobugarin@gmail.com>
2022-12-10 18:26:47 -0800

    updated userAddresses schema --followed usps address validation props

--- models/userAddresses.js
+++ models/userAddresses.js
@@ -8,11 +8,13 @@ module.exports = mongoose.model('UserAddresses', mongoose.Schema(
             ref: 'User'
         },
         addresses: [{
-            name: { type: String, required: true},
-            address: { type: String, required: true},
-            city: { type: String, required: true},
-            postalCode: { type: String, required: true},
-            country: { type: String, required: true},
+            name: { type: Object, required: true},
+            Address2: { type: String, required: true},
+            Address1: { type: String, required: false},
+            City: { type: String, required: true},
+            State: { type: String, required: true},
+            Zip5: { type: String, required: true},
+            Zip4: { type: String, required: true},
         }],
     }, {
         timestamps: true
