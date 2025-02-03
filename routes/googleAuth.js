077b4892814a58ae1a21f637022219205074d744
paoloBugarin <paolopaolobugarin@gmail.com>
2025-02-02 19:16:54 -0800

    updated googleAuth

--- routes/googleAuth.js
+++ routes/googleAuth.js
@@ -2,8 +2,8 @@ const express = require("express");
 const asyncHandler = require("express-async-handler");
 const router = express.Router();
 const generateToken = require("../utils/generateToken");
-const cookieJwtAuth = require("../middlewares/cookieJwtAuth");
-const bcrypt = require("bcryptjs");
+// const cookieJwtAuth = require("../middlewares/cookieJwtAuth");
+// const bcrypt = require("bcryptjs");
 const User = require("../models/userModel");
 const { OAuth2Client, getDecodedOAuthJwtGoogle } = require('google-auth-library');
 
